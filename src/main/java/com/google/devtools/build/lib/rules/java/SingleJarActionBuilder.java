1caa4360e7900ee6c4c7798694ebe8f3be086406
Mansheng Yang <myang@dytechlab.com>
2017-07-07 09:11:00 +0800

    singlejar should use default env to avoid missing so

--- src/main/java/com/google/devtools/build/lib/rules/java/SingleJarActionBuilder.java
+++ src/main/java/com/google/devtools/build/lib/rules/java/SingleJarActionBuilder.java
@@ -121,6 +121,7 @@ public final class SingleJarActionBuilder {
             .addOutput(outputJar)
             .addInputs(resources)
             .addTransitiveInputs(resourceJars)
+            .useDefaultShellEnvironment()
             .addCommandLine(
                 sourceJarCommandLine(outputJar, semantics, resources, resourceJars),
                 ParamFileInfo.builder(ParameterFileType.SHELL_QUOTED).setUseAlways(true).build())
@@ -146,6 +147,7 @@ public final class SingleJarActionBuilder {
                 JavaToolchainProvider.from(ruleContext), JavaRuntimeInfo.forHost(ruleContext))
             .addOutput(output)
             .addInputs(jars)
+            .useDefaultShellEnvironment()
             .addCommandLine(
                 sourceJarCommandLine(
                     output,
