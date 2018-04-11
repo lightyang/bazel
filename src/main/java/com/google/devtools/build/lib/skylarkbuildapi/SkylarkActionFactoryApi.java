733b93396882dc5cd747c8006975108b07a2bc9d
Mansheng Yang <myang@dytechlab.com>
2018-04-11 15:11:32 +0800

    change default value of use_default_shell_env to true

--- src/main/java/com/google/devtools/build/lib/skylarkbuildapi/SkylarkActionFactoryApi.java
+++ src/main/java/com/google/devtools/build/lib/skylarkbuildapi/SkylarkActionFactoryApi.java
@@ -241,7 +241,7 @@ public interface SkylarkActionFactoryApi extends SkylarkValue {
         @Param(
             name = "use_default_shell_env",
             type = Boolean.class,
-            defaultValue = "False",
+            defaultValue = "True",
             named = true,
             positional = false,
             doc = "Whether the action should use the built in shell environment or not."),
@@ -408,7 +408,7 @@ public interface SkylarkActionFactoryApi extends SkylarkValue {
         @Param(
             name = "use_default_shell_env",
             type = Boolean.class,
-            defaultValue = "False",
+            defaultValue = "True",
             named = true,
             positional = false,
             doc = "Whether the action should use the built in shell environment or not."),
